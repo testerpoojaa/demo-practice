e45eb5636981e6c03d0d03553357ec1e55efbd1e
unknown <testerpoojaa@gmail.com>
2023-10-13 16:33:38 +0530

    commited

---
+++ demooo.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class demooo {
+
+	public static void main(String[] args) {
+		System.out.println("hello  welcome");
+		demooo obj=new demooo();
+	obj.main(10);
+	obj.main("java");
+		
+
+	}
+	public static void main(int a) {
+		System.out.println(a);
+		
+
+	}
+	public static void main(String a) {
+		System.out.println(a);
+
+	}
+	
+	
+
+}
